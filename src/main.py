4447248a463c4dfc4c256f2d9c683945a6c3e551
dulchik <bubnyukab@gmail.com>
2025-05-22 22:36:08 +0200

    Revert "added testunits"
    This reverts commit a7fed682e1e28330a65532f01e193411b7e1bf33.

--- src/main.py
+++
@@ -1,9 +0,0 @@
-from textnode import TextNode, TextType
-
-def main():
-    t = TextNode("This is some anchor text", TextType.LINK, "https://www.boot.dev")
-    print(t)
-
-
-if __name__ == "__main__":
-    main()
